dee69d88b3c4447d0be0a76a077a03011f569653
Mr. Bartucz <jobartucz@isd535.org>
2019-01-29 09:56:38 -0600

    Update MySQLAccess.java

--- MySQLAccess.java
+++ MySQLAccess.java
@@ -16,13 +16,13 @@ public class MySQLAccess {
         try {
             // This will load the MySQL driver, each DB has its own driver
             Class.forName("com.mysql.jdbc.Driver");
-            // Setup the connection with the DB
-            connect = DriverManager.getConnection("jdbc:mysql://localhost/University?user=SQLUSER&password=SQLPASSWORD");
+            // Setup the connection with the test DataBase - EVERYONE HAS ACCESS, PLEASE BE CAREFUL!!
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/University?user=CS485&password=WinonaState");
 
             // Statements allow to issue SQL queries to the database
             statement = connect.createStatement();
             // Result set get the result of the SQL query
-            resultSet = statement.executeQuery("select * from University.Students");
+            resultSet = statement.executeQuery("select * from CS485.Students");
             writeResultSet(resultSet);
 
         } catch (Exception e) {
